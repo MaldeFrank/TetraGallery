45b76896c3523dd034f2ec263cc8877839797e01
Malde <maldefrankmidtgaard@gmail.com>
2025-10-02 18:40:54 +0200

    init

---
+++ src/main/java/org/art/tetragallery/TetraGalleryApplication.java
@@ -0,0 +1,13 @@
+package org.art.tetragallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetraGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TetraGalleryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/art/tetragallery/TetraGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.art.tetragallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetraGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

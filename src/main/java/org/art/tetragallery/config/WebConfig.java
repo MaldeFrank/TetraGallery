a7608c9b40981949bf598e72a54d9a9674c21bc0
FrederikVA <s224308@student.dtu.dk>
2025-11-19 17:13:50 +0100

    Create WebConfig.java
    To allow localhost fetch for frontend - development

---
+++ src/main/java/org/art/tetragallery/config/WebConfig.java
@@ -0,0 +1,29 @@
+package org.art.tetragallery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173"   // Vite dev
+                                // ,"https://our-frontend.com" // later
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+                        // If we ever send cookies from backend:
+                        // .allowCredentials(true);
+            }
+        };
+    }
+}
